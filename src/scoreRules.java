240ba1ef797bbbaff6784346cdcf086f63fb9c62
VascoJ2000 <vascojoao2000escola@gmail.com>
2022-12-31 17:38:22 +0000

    v0.1.2.1 pre-alpha

--- src/scoreRules.java
+++ src/scoreRules.java
@@ -1,15 +1,15 @@
 //Class para criar as regras de jogo. Metodos para ver se um jogador ganhou, se marcou ponto.
 public class scoreRules {
-    private int boardSize; //Tamanho do jogo
-    private int score; //Nº de quadrados seguidos para marcar pontos
-    private int winScore; //Nº de pontos para vencer. se -1 deixar o tabuleiro ser preenchido.
+    final private int boardSize; //Tamanho do jogo
+    final private int score; //Nº de quadrados seguidos para marcar pontos
+    final private int winScore; //Nº de pontos para vencer. se -1 deixar o tabuleiro ser preenchido.
 
     public scoreRules(int boardSize, int score, int winScore) {
         this.boardSize = boardSize;
         this.score = score;
         this.winScore = winScore;
     }
-    //Metodo que virifica se marcou ponto.
+    //Metodo que verifica se marcou ponto.
     public boolean hasScored(int position){
         return false;
     }
